d5c2aa401d2e0e02b5ae6ecb97a4c519ac993464
HongjianHuang <huanghongjian@live.cn>
2023-05-25 22:43:25 -0400

    add pic and parallax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useRef } from "react";
+import moon from "./FullMoon2010.jpg";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+//import { Parallax } from "react-parallax";
 function App() {
+  //const ref = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Parallax
+        pages={4}
+        style={{ backgroundImage: "linear-gradient(#e66465, #9198e5)" }}
+      >
+        {/* <div className="content">
+          <div className="text-content">Normal Parallax</div>
+        </div> */}
+        <ParallaxLayer
+          offset={0}
+          //speed={0.1}
+          className="center"
+          factor={1.2}
+          style={{ backgroundColor: "#FFCDD2" }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="text-content">Normal Parallax</div>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={0}
+          speed={16}
+          className="center"
+          style={{ backgroundColor: "#EF9A9A" }}
+        >
+          <div className="text-content">Normal Parallax</div>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={1.2}
+          //speed={12}
+          className="center"
+          factor={0.9}
+          style={{ backgroundColor: "#E57373" }}
+        >
+          <div className="text-content">Normal Parallax</div>
+        </ParallaxLayer>
+      </Parallax>
+      {/* <div className="content"></div> */}
     </div>
   );
 }
